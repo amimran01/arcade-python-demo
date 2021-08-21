93b7f262b5903158a1bc899d6c6bf0a43aab7fe1
Alexis MIMRAN <84452677+amimran01@users.noreply.github.com>
2021-08-21 22:50:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from requests import get
 import os
+from requests import get
 
+"""
+Test code
+"""
 name = os.environ['PYTHON_VAR_username']
 ip = get('https://api.ipify.org').text
 print(f'Hello {name}, your public IP is: {ip}')

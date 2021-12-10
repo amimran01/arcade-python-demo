e2ba1cce94b641515a27c00d215a1070321e383a
Alexis MIMRAN <84452677+amimran01@users.noreply.github.com>
2021-12-10 12:33:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import time
 from requests import get
 
 """
@@ -8,5 +9,7 @@ Test code
 name = os.environ['PYTHON_VAR_username']
 ip = get('https://api.ipify.org').text
 for x in range(10):
+  time.sleep(0.2)
   print(f'Stdout : Hello {name}, your public IP is: {ip}')
+  time.sleep(0.2)
   sys.stderr.write(f'Stderr : Hello {name}, your public IP is: {ip}\n')
